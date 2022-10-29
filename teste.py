fd2a07172bb90390dd1a1d116befa27d0621c1a1
Timewound <rubenjms1@gmail.com>
2022-10-29 19:10:23 -0300

    adicionando pages do sistema

--- teste.py
+++ teste.py
@@ -6,8 +6,11 @@ class Novo(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
         super().__init__(parent)
         super().setupUi(self)
-        
-        
+        self.actionCadastrar.triggered.connect(lambda: self.stackedWidget.setCurrentWidget(self.register_2))
+        self.actionEmail.triggered.connect(lambda: self.stackedWidget.setCurrentWidget(self.update))
+        self.actionRegistrar.triggered.connect(lambda: self.stackedWidget.setCurrentWidget(self.pay))
+        self.actionPesquisar.triggered.connect(lambda: self.stackedWidget.setCurrentWidget(self.search))
+        self.actionExcluir.triggered.connect(lambda: self.stackedWidget.setCurrentWidget(self.delete_2))
         
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
